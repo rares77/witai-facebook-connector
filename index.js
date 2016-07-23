7b01a380f1770a72ab0f91455b56d8dddf2d6838
Rares Ilea <raresilea@skynetcorp.ro>
2016-07-23 14:29:11 +0300

    adding bootstrap and main page

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bootstrap = require('bootstrap');
+
+app.set('port', (process.env.PORT || 5000));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
